968698a4ae2c95645e97e7a7abc92b27c1faa987
Illia Smyhunov <illia.smyhunov@jetsoftpro.com>
2023-06-27 19:11:56 +0300

    Implement a job finder from dou.ua

---
+++ app/selenium_job_finder/job_finder.py
@@ -0,0 +1,77 @@
+import csv
+import os
+import time
+import logging
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+logger = logging.getLogger(__name__)
+
+
+URL = "https://jobs.dou.ua/"
+
+
+def get_driver():
+    chromedriver_path = "./chromedriver"
+    options = Options()
+    options.add_experimental_option("detach", True)
+    options.add_argument("--headless")
+    service = Service(
+        executable_path=chromedriver_path)
+    driver = Chrome(service=service, options=options)
+    return driver
+
+
+def find_jobs(category: str, additional_info: str | None = None):
+    driver = get_driver()
+    driver.get(URL)
+    all_category_links = driver.find_element(
+        By.CLASS_NAME, "b-recent-searches")
+    try:
+        all_category_links.find_element(
+            By.LINK_TEXT, category).click()
+        if additional_info:
+            driver.find_element(
+                By.XPATH, "//input[@name='search']").send_keys(additional_info)
+            driver.find_element(By.CLASS_NAME, "btn-search").click()
+
+        show_more = driver.find_element(
+            By.XPATH, "//div[@class='more-btn']/a")
+        while show_more.is_displayed():
+            time.sleep(1)
+            show_more.click()
+            time.sleep(1)
+
+    except NoSuchElementException:
+        pass
+
+    vacancy_elements = driver.find_elements(By.CLASS_NAME, "l-vacancy")
+    results = []
+
+    # Проходимося по кожному елементу вакансії та отримуємо назву та компанію
+    for vacancy_element in vacancy_elements:
+        title_element = vacancy_element.find_element(By.CLASS_NAME, "vt")
+        title = title_element.text
+        link = title_element.get_attribute("href")
+        company = vacancy_element.find_element(By.CLASS_NAME, "company").text
+        results.append({
+            "title": title,
+            "company": company,
+            "link": link,
+        })
+
+    fieldnames = ["title", "company", "link"]
+    filename = "vacancies.csv"
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()  # Записуємо заголовки стовпців
+        writer.writerows(results)  # Записуємо рядки даних
+
+    driver.quit()
+
+    return filename
---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, send_file
+
+from app.selenium_job_finder.job_finder import find_jobs
+
+app = Flask(__name__, template_folder="app/templates")
+
+
+@app.route("/", methods=["GET", "POST"])
+def get_jobs():
+    if request.method == "POST":
+        category = request.form.get("category")
+        additional_info = request.form.get("additional_info")
+
+        if not request.form or not category:
+            return render_template(
+                "error.html", message="Категорія обов'язкова для заповнення!",
+                back_url="/"), 400
+
+        filename = find_jobs(
+            category=category, additional_info=additional_info)
+        return send_file(filename)
+    return render_template("form.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
